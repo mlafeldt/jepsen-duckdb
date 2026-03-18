74ad4ced6b053980a904521ee395373912f599ad
Kyle Kingsbury <aphyr@aphyr.com>
2026-03-18 13:03:28 -0500

    Jepsen 0.3.12-SNAPSHOT

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.12.4"]
-                 [jepsen "0.3.11-SNAPSHOT"]
+                 [jepsen "0.3.12-SNAPSHOT"]
                  [clj-http "3.13.1"]]
   :main jepsen.duckdb.cli
   :repl-options {:init-ns jepsen.duckdb.repl}
